91244f96fb2483cbbe36ffc44f4a4ec3036eb2de
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-07-29 07:45:55 +0100

    Updates - Browserslist - Attribution

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,10 +1,10 @@
-
 const Attribution = () => {
   return (
     <div className="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-    Coded by <a href="#">DesigNeriS</a>.
-  </div>
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+      Coded by <a href="https://www.designreris.net">DesigNeriS</a>.
+    </div>
   )
 }
 
